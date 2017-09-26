729dcf9b706d9272202b400d25cdce751fec7f2e
liuyouxiao <694215916@qq.com>
2017-09-26 11:38:44 +0800

    add 1.c

---
+++ 1.c
@@ -0,0 +1,15 @@
+/*************************************************************************
+	> File Name: 1.c
+	> Author: lyx
+	> Mail: 694215916@qq.com 
+	> Created Time: 2017年09月26日 星期二 11时36分32秒
+ ************************************************************************/
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+int main(int argc, char *argv[])
+{
+	return 0;
+}
+
